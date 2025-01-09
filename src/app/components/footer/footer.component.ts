14b65c38b129252d9c46b32bf3ff97a4206b960d
caegomezda <camiloedg23@gmail.com>
2025-01-09 04:13:55 -0500

    Fix

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -12,7 +12,7 @@ export class FooterComponent implements OnInit {
   constructor(private translationService: TranslationService) {}
 
   ngOnInit(): void {
-    this.translationService.currentTranslations.subscribe((translations) => {
+    this.translationService.currentTranslations.subscribe((translations :any) => {
       this.translations = translations?.footer || {};
     });
   }
