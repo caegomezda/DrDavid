172e832ca1acdfe51581eaf526aa02c70a8265e4
caegomezda <camiloedg23@gmail.com>
2025-01-10 11:01:27 -0500

    Loading funcionalidad en formulario de citas

--- src/app/components/appointment-scheduler/appointment-scheduler.component.ts
+++ src/app/components/appointment-scheduler/appointment-scheduler.component.ts
@@ -8,11 +8,10 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./appointment-scheduler.component.scss'],
 })
 export class AppointmentSchedulerComponent implements OnInit {
-  private url = 'https://script.google.com/macros/s/AKfycbzaqtubg5FAMEZrtJHg9wsq8M0XSvaD9x_6Cu54KwG6wrOddDd7F8zo6fkmdmNceEKseA/exec';
   currentForm: { [key: string]: any } = {};
   translations: any = {};
   appointmentForm = {
-    currentDate:'',
+    currentDate: '',
     name: '',
     documentType: '',
     documentNumber: '',
@@ -21,33 +20,59 @@ export class AppointmentSchedulerComponent implements OnInit {
     policy: '',
   };
 
+  loading: boolean = false; // Nueva bandera de estado de carga
+  successMessage: string = ''; // Mensaje de éxito
+  errorMessage:string = '' // Limpiar mensaje de error previo
+  formSubmitted: boolean = false; // Nueva propiedad para rastrear el estado de envío
+  formError: boolean = false; // Nueva propiedad para rastrear el estado de envío
+  
   policies = ['Sura Salud Para Todos', 'Sura Global', 'Sura Clásico', 'Particular'];
   contactMethods = ['Email', 'Phone', 'WhatsApp'];
 
   constructor(
     private translationService: TranslationService,
-    // private formStorageService: FormStorageService,
     private http: HttpClient
   ) {}
 
   ngOnInit(): void {
-    this.translationService.currentTranslations.subscribe((translations :any) => {
+    this.translationService.currentTranslations.subscribe((translations: any) => {
       this.translations = translations?.appointmentScheduler || {};
     });
   }
-  async submitForm(): Promise<void> {
-    this.appointmentForm.currentDate = await new Date().toISOString(); 
-    const url = '/api'; // URL del proxy configurado
-    const formData = { ...this.appointmentForm };
-    console.log("formulario",formData);
-    
-    try {
-      // const response = await this.http.post(url, formData).toPromise();
-      const response = await this.http.post(url, this.appointmentForm ).toPromise();
 
+  async submitForm(): Promise<void> {
+    this.appointmentForm.currentDate = await new Date().toISOString();
+    // const url = 'https://script.google.com/macros/s/AKfycbyv2fuxnbYd4ouyGehHzjEFMuxuwwLkOlzbOF9_CVyXpe474OcNFP-Wj2b_XX85kdzi9w/exec'; // URL del proxy configurado
+    const url = '/api'; // URL del proxy configurado
+    this.loading = true; // Activar el estado de carga
+    this.successMessage = ''; // Limpiar cualquier mensaje previo
+    this.errorMessage  = ''; // Limpiar mensaje de error previo
+    const formData = { ...this.appointmentForm };
+    try {
+      const response = await this.http.post(url, this.appointmentForm).toPromise();
       console.log('Respuesta del servidor:', response);
+      // Mostrar mensaje de éxito
+      this.successMessage = this.translations?.formSubmitted || 'Form submitted successfully';
+      // Limpiar el formulario
+    this.resetForm();
     } catch (error) {
       console.error('Error al enviar el formulario:', error);
+      // Mostrar mensaje de error
+      this.errorMessage = this.translations?.formError || 'An error occurred while submitting the form. Please try again.';
+    } finally {
+      this.loading = false; // Desactivar el estado de carga
     }
+  }
+
+  resetForm(): void {
+    this.appointmentForm = {
+      currentDate: '',
+      name: '',
+      documentType: '',
+      documentNumber: '',
+      phone: '',
+      contactMethod: '',
+      policy: '',
+    };
   }
 }
